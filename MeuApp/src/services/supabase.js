50ed9d273b2f04c2cc563daf9dff0bae101c7f9f
JoaoGustavoMr <joaogustavo2202@gmail.com>
2025-11-04 09:11:46 -0300

    config supabase

--- MeuApp/src/services/supabase.js
+++ MeuApp/src/services/supabase.js
@@ -4,8 +4,8 @@ import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // 🚨 SUBSTITUA PELAS SUAS CHAVES DO SUPABASE 🚨
-const supabaseUrl = 'SEU_URL_DO_SUPABASE';
-const supabaseAnonKey = 'SUA_CHAVE_ANON';
+const supabaseUrl = 'https://gqzbwapvquqofdaisbje.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxemJ3YXB2cXVxb2ZkYWlzYmplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNDM3NTEsImV4cCI6MjA3NzgxOTc1MX0._qH_VLNZmxgo3Q2vUy0W5UGral3khslB3scz5Z_Fwg8';
 
 // Usa AsyncStorage para persistir a sessão de usuário
 const Supabase = createClient(supabaseUrl, supabaseAnonKey, {
